ea25bccba297f37ebd37c617e1a29c51130f7a15
Carlos Martinez-Villar <cimcrd@umsystem.edu>
2023-06-23 01:02:28 -0500

    missing a lib

--- download.py
+++ download.py
@@ -6,6 +6,7 @@ import argparse
 from tqdm import tqdm
 import time
 from multiprocessing import Pool
+import sys
 
 ####################################################################################################
 # GLOBAL VARIABLES
